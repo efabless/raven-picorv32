2d6b6d1bf620b0fb2123f28c72f8168d2922f5f6
Mohamed Kassem <mkk+github@efabless.com>
2019-03-10 11:46:54 -0700

    updated compile files

--- verilog/raven_defs.h
+++ verilog/raven_defs.h
@@ -10,6 +10,9 @@
 // know that because "sram" is a linker symbol from sections.lds.
 extern uint32_t sram;
 
+extern uint32_t flashio_worker_begin;
+extern uint32_t flashio_worker_end;
+
 // Mermory map definitions
 
 #define reg_spictrl (*(volatile uint32_t*)0x02000000)
